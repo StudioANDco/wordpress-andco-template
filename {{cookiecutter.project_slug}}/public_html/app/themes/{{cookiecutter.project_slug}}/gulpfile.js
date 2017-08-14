46887ad751b71aa828ddbe13ae158b2f47f69c79
Gilles Crettenand <gilles.crettenand@liip.ch>
2017-08-14 10:39:18 +0200

    autoprefixer

--- {{cookiecutter.project_slug}}/public_html/app/themes/{{cookiecutter.project_slug}}/gulpfile.js
+++ {{cookiecutter.project_slug}}/public_html/app/themes/{{cookiecutter.project_slug}}/gulpfile.js
@@ -4,12 +4,21 @@ var reload      = browserSync.reload;
 var sass        = require('gulp-sass');
 var concat      = require('gulp-concat');
 var uglify      = require('gulp-uglify');
+var rename      = require('gulp-rename');
+var svgSymbols  = require('gulp-svg-symbols');
+var runSequence = require('run-sequence');
+var prefix      = require('gulp-autoprefixer');
 
 var src = {
   scss: 'assets/scss/**/*.scss',
   css:  'assets/stylesheets/',
+  icons: 'assets/icons/*.svg',
   php:  '**/*.php',
   js:   'assets/javascripts/**/*.js'
+};
+
+var config = {
+    browsers: 'last 2 versions, ie 9'
 };
 
 /**
@@ -20,6 +29,7 @@ gulp.task('watch', ['sass'], function() {
     proxy: 'andco.lo'
   });
 
+  gulp.watch(src.icons, ['icons:watch']);
   gulp.watch(src.scss, ['sass']);
   gulp.watch(src.js, ['scripts']);
   gulp.watch(src.php).on('change', reload);
@@ -33,6 +43,7 @@ gulp.task('sass', function() {
     .pipe(sass({
       outputStyle: 'compressed'
     }).on('error', sass.logError))
+    .pipe(prefix(config.browsers))
     .pipe(gulp.dest(src.css))
     .pipe(reload({stream: true}));
 });
@@ -49,3 +60,15 @@ gulp.task('scripts', function() {
 });
 
 gulp.task('default', ['watch']);
+
+/** SVG icons */
+gulp.task('icons:watch', function() {
+    runSequence('icons', reload);
+});
+
+gulp.task('icons', function() {
+    return gulp.src(src.icons)
+        .pipe(svgSymbols({ templates: ['assets/icons/templates/icons.svg'] }))
+        .pipe(rename('icons.svg'))
+        .pipe(gulp.dest('assets/images/'));
+});
